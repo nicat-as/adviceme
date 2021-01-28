59ff1e97ff8d093ba19f456cc02db3e7536f7661
Nicat Asgerzade <nicat.asgerzade.9889@gmail.com>
2021-01-29 00:26:25 +0400

    EQ-1: Closes #1

---
+++ src/test/java/com/uniso/equso/EqusoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniso.equso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EqusoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
