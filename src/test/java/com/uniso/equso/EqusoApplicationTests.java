f786121b5008778205f4e3dfe63e432893b1c803
Nicat Asgerzade <nicat.asgerzade.9889@gmail.com>
2021-01-30 23:23:32 +0400

    add test

--- src/test/java/com/uniso/equso/EqusoApplicationTests.java
+++ src/test/java/com/uniso/equso/EqusoApplicationTests.java
@@ -1,9 +1,11 @@
 package com.uniso.equso;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootTest
+
 class EqusoApplicationTests {
 
     @Test
