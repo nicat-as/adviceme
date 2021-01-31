c6889ac7de51fe5642710ede1ec206645ef54c63
Nicat Asgerzade <nicat.asgerzade.9889@gmail.com>
2021-02-01 02:32:01 +0400

    EQ-12: Add dao entities and repositories

---
+++ src/main/java/com/uniso/equso/dao/entities/CommentEntity.java
@@ -0,0 +1,48 @@
+package com.uniso.equso.dao.entities;
+
+import com.uniso.equso.dao.enums.Status;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comment")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = {"id"})
+public class CommentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    @Type(type = "org.hibernate.type.TextType")
+    @Column(nullable = false)
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "creator_id")
+    private UserEntity creator;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private PostEntity post;
+
+    @CreationTimestamp
+    @Column(name = "created_at",updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "last_updated_at")
+    private LocalDateTime lastUpdatedAt;
+
+    @Enumerated(value = EnumType.STRING)
+    private Status status;
+
+}
