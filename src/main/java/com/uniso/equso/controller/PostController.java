67868116beca4b586807e56fe030bbabdbcede30
Nicat Asgerzade <nicat.asgerzade.9889@gmail.com>
2021-01-31 18:33:34 +0400

    fix error

--- src/main/java/com/uniso/equso/controller/PostController.java
+++ src/main/java/com/uniso/equso/controller/PostController.java
@@ -1,26 +1,28 @@
 package com.uniso.equso.controller;
 
 import com.uniso.equso.model.CreatePostRequest;
+import com.uniso.equso.model.PostDto;
 import com.uniso.equso.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("${url.root}/post")
+@RestController
+@RequestMapping("${url.root}/post")
 @RequiredArgsConstructor
 public class PostController {
     private final PostService postService;
 
     @PostMapping
-    public ResponseEntity createPost(@RequestBody CreatePostRequest request){
+    public ResponseEntity<Void> createPost(@RequestBody CreatePostRequest request){
         postService.createPost(request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @GetMapping("{id}")
-    public ResponseEntity getPostById(@PathVariable Long id){
-        return ResponseEntity.ok(postService.getPost(id));
+    @GetMapping("{postId}")
+    public ResponseEntity<PostDto> getPostById(@PathVariable Long postId){
+        return ResponseEntity.ok(postService.getPost(postId));
     }
 
 }
