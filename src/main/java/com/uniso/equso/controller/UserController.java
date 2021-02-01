0fdf1a904888e1934ad3071e79be6b752acee37e
Nicat Asgerzade <nicat.asgerzade.9889@gmail.com>
2021-02-01 04:50:07 +0400

    EQ-13: Closes #12

--- src/main/java/com/uniso/equso/controller/UserController.java
+++ src/main/java/com/uniso/equso/controller/UserController.java
@@ -1,10 +1,10 @@
 package com.uniso.equso.controller;
 
-import com.uniso.equso.config.security.CustomUserDetails;
 import com.uniso.equso.model.CheckEmailResponse;
 import com.uniso.equso.model.UserDto;
 import com.uniso.equso.service.UserService;
 import com.uniso.equso.util.AuthenticationUtil;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -13,21 +13,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import javax.validation.constraints.Email;
 
 @RestController
 @RequestMapping("${url.root}/user")
+@RequiredArgsConstructor
 @Slf4j
 @Validated
 public class UserController {
     private final UserService userService;
     private final AuthenticationUtil authenticationUtil;
 
-    public UserController(UserService userService, AuthenticationUtil authenticationUtil) {
-        this.userService = userService;
-        this.authenticationUtil = authenticationUtil;
-    }
 
     @GetMapping("profile")
     public ResponseEntity<UserDto> getUserProfile() {
@@ -36,7 +32,7 @@ public class UserController {
     }
 
     @GetMapping("email")
-    public ResponseEntity<CheckEmailResponse> checkEmail(@RequestParam("q") @Email String query){
+    public ResponseEntity<CheckEmailResponse> checkEmail(@RequestParam("q") @Email String query) {
         return ResponseEntity.ok(userService.isValidEmail(query));
     }
 
