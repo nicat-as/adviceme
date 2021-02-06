94ad3644d5f6289a08ce8ddf72e2cc8bba4cb398
Nicat Asgerzade <nicat.asgerzade.9889@gmail.com>
2021-02-06 23:28:56 +0400

    EQ-20: Implement semantic versioning; Change api info in swagger

--- src/main/java/com/uniso/equso/config/SwaggerConfig.java
+++ src/main/java/com/uniso/equso/config/SwaggerConfig.java
@@ -1,23 +1,28 @@
 package com.uniso.equso.config;
 
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
 import java.util.Collections;
 import java.util.List;
+import java.util.Properties;
 import java.util.function.Predicate;
 
 @Configuration
 @EnableSwagger2
+@RequiredArgsConstructor
 public class SwaggerConfig {
     // Swagger url example: http://localhost:8081/swagger-ui/
 
@@ -27,9 +32,12 @@ public class SwaggerConfig {
     private static final String SCOPE = "global";
     private static final String DESC = "accessEverything";
 
+    private final ApiInfoConfig apiInfoConfig;
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getInfo())
                 .securityContexts(List.of(securityContext()))
                 .securitySchemes(List.of(apiKey()))
                 .select()
@@ -55,4 +63,16 @@ public class SwaggerConfig {
         authorizationScopes[0] = authorizationScope;
         return Collections.singletonList(new SecurityReference(JWT, authorizationScopes));
     }
+
+    private ApiInfo getInfo(){
+        System.out.println(apiInfoConfig);
+        return new ApiInfoBuilder()
+                .title(apiInfoConfig.getTitle())
+                .version(apiInfoConfig.getVersion())
+                .description(apiInfoConfig.getDescription())
+                .build();
+    }
+
+
+
 }
