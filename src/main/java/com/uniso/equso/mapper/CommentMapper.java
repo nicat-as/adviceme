ce4dd7aae00e2d0cb6579a7f67451acf526d17c8
Nicat Asgerzade <nicat.asgerzade.9889@gmail.com>
2021-02-21 20:38:12 +0400

    fix delete comment

--- src/main/java/com/uniso/equso/mapper/CommentMapper.java
+++ src/main/java/com/uniso/equso/mapper/CommentMapper.java
@@ -1,11 +1,12 @@
 package com.uniso.equso.mapper;
 
 import com.uniso.equso.dao.entities.CommentEntity;
-import com.uniso.equso.dao.entities.PostEntity;
-import com.uniso.equso.model.comments.CommentCreateRequest;
+import com.uniso.equso.dao.enums.Status;
 import com.uniso.equso.model.comments.CommentResponseDto;
+import org.mapstruct.BeforeMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -14,7 +15,16 @@ import java.util.List;
 public abstract class CommentMapper {
     public static final CommentMapper INSTANCE = Mappers.getMapper(CommentMapper.class);
 
-    @Mapping(target = "creator",expression = "java(UserMapper.INSTANCE.entityToUserInfo(entity.getCreator()))")
+    @Mapping(target = "creator", expression = "java(UserMapper.INSTANCE.entityToUserInfo(entity.getCreator()))")
     public abstract List<CommentResponseDto> entityListToCommentResponseDtoList(List<CommentEntity> entity);
+
+    @BeforeMapping
+    protected void filterComments(List<CommentEntity> comments, @MappingTarget List<CommentResponseDto> responseDtos) {
+        for (int i = 0; i < comments.size(); i++) {
+            if (comments.get(i).getStatus() != Status.ACTIVE) {
+                comments.remove(i);
+            }
+        }
+    }
 
 }
